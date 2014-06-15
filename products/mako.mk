82ea5a12e00dfc9d149da953a97f6816499eabc4
TheByteSmasher <thebytesmasher@gmail.com>
2014-06-15 16:31:26 -0400

    attempt to OurROM-ify more

--- products/mako.mk
+++ products/mako.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 #PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/mako
 
 # Setup device specific product configuration
-PRODUCT_NAME := aokp_mako
+PRODUCT_NAME := ourrom_mako
 PRODUCT_BRAND := Google
 PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
@@ -20,4 +20,4 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4.3/KTU84L/1148727:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.3 KTU84L 1148727 release-keys"
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_720.zip:system/media/bootanimation.zip
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1280.zip:system/media/bootanimation.zip
